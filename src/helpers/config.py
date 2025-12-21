9db372605a976c4afc70e3a895cc4dd5a5cc4115
Yahia Hamitou <hamitou.yah@gmail.com>
2025-12-21 21:35:43 +0100

    Add configuration management and MVC structure

---
+++ src/helpers/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str
+    class Config:
+        env_file = ".env"
+
+
+def get_settings():
+    return Settings()
