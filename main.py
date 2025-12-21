2d0facfad903f259f488da080f0cd66fd27259f0
Yahia Hamitou <hamitou.yah@gmail.com>
2025-12-21 19:47:20 +0100

    Add API routing structure

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+from routes import base
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "IIR - Intelligent Information Retrieval",
-    }
+app.include_router(base.base_router)
