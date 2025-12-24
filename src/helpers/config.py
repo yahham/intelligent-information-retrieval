a6f254a25c596e1189917e5dfb381cf65e8547cf
Yahia Hamitou <hamitou.yah@gmail.com>
2025-12-24 19:59:43 +0100

    Add application settings and environment configuration

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,9 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    
     class Config:
         env_file = ".env"
 
